0eea64a614bce93197a2005b398ba72d7e9ac9c6
curso <curso@curso.com>
2022-07-19 21:02:20 -0300

    Terminamos a implementação da calculadora.

--- app/src/main/java/br/com/impacta/projetocalculadora/MainActivity.kt
+++ app/src/main/java/br/com/impacta/projetocalculadora/MainActivity.kt
@@ -49,21 +49,13 @@ class MainActivity : AppCompatActivity() {
         binding.btnNumPonto.setOnClickListener { digitaNumero(".") }
         binding.btnNumApagar.setOnClickListener { digitaNumero("-1") }
 
-        binding.btnOprAdicao.setOnClickListener {
-            digitaOperacao("+")
-        }
-        binding.btnOprSubtracao.setOnClickListener {
-            digitaOperacao("-")
-        }
-        binding.btnOprMultiplicacao.setOnClickListener {
-            digitaOperacao("*")
-        }
-        binding.btnOprDivisao.setOnClickListener {
-            digitaOperacao("/")
-        }
-        binding.btnOprPorcetagem.setOnClickListener {
-            digitaOperacao("%")
-        }
+        binding.btnOprAdicao.setOnClickListener { digitaOperacao("+") }
+        binding.btnOprSubtracao.setOnClickListener { digitaOperacao("-") }
+        binding.btnOprMultiplicacao.setOnClickListener { digitaOperacao("*") }
+        binding.btnOprDivisao.setOnClickListener { digitaOperacao("/") }
+        binding.btnOprPorcetagem.setOnClickListener { digitaOperacao("%") }
+
+        binding.btnOprIgual.setOnClickListener { mostraResultado() }
     }
 
     fun digitaNumero(opcao: String) {
@@ -100,20 +92,47 @@ class MainActivity : AppCompatActivity() {
     fun digitaOperacao(opcao: String) {
         operacaoSelecionada = opcao
 
+        // Pegamos o número digitado pelo usuário
         var linhaInferior = binding.textLinhaInferior.text.toString()
+        // Neste if tratamos o caso de o usuário apertar duas operações seguidas.
         var linhaSuperior = if (linhaInferior != "0") {
+            // Só reatribuimos um novo valor para a variavel ultimoValor caso o número digitado
+            // seja diferente de 0
             ultimoValor = linhaInferior.toDouble()
             "$linhaInferior $opcao"
         } else {
+            // Aqui, não houve uma entrada de número, então podemos simplesmente trocar a operação
             "$ultimoValor $opcao"
         }
 
+        // Mostramos as novas informações nos campos de texto
         binding.textLinhaSuperior.text = linhaSuperior
         binding.textLinhaInferior.text = "0"
     }
 
+
     fun mostraResultado() {
-//        when (opcao)
+        // Pego o novo número digitado após a entrada da operação
+        var numero2 = binding.textLinhaInferior.text.toString().toDouble()
+        // Faço o cálculo da operação
+        // pegando o valor da variavel ultimoValor e verificando se é diferente de null
+        // caso seja diferente de null, executamos o cálculo
+        // Funcionamento do LET: O let verifica uma condição a esquerda, caso se verdadeira,
+        // pega o valor da condição e executa o trecho de código entre { }
+        // e retorna o valor do mesmo tipo da condição, o último valor que ele encontrar dentro
+        // do código que foi executado.
+        var resultado = ultimoValor?.let { numero1 ->
+            when(operacaoSelecionada) {
+                "+" -> calculaValorFinal(numero1, numero2, adicao)
+                "-" -> calculaValorFinal(numero1, numero2, subtracao)
+                "*" -> calculaValorFinal(numero1, numero2, multiplicacao)
+                "/" -> calculaValorFinal(numero1, numero2, divisao)
+                else -> calculaValorFinal(numero1, numero2, porcentagem)
+            }
+        }
+        // atribuimos os novos valores aos campos de texto.
+        binding.textLinhaSuperior.text = ""
+        binding.textLinhaInferior.text = resultado.toString()
     }
 
 }
